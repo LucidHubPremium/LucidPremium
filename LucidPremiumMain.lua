f589c56e85f3793eef6036396821b570e6e14022
LucidHubPremium <121714664+LucidHubPremium@users.noreply.github.com>
2023-07-24 07:58:31 -0930

    Create LucidPremiumMain.lua

---
+++ LucidPremiumMain.lua
@@ -0,0 +1,7 @@
+local gameId = game.GameId
+
+if gameId == 8204899140 then
+
+end
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LucidHubPremium/LucidPremium/main/Lucid-Premium-FF2"))()
+   
