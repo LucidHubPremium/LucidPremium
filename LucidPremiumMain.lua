06fc86f085231a281efce5a5f40df984b755f8b1
LucidHubPremium <121714664+LucidHubPremium@users.noreply.github.com>
2023-07-24 08:10:30 -0930

    Update LucidPremiumMain.lua

--- LucidPremiumMain.lua
+++ LucidPremiumMain.lua
@@ -3,5 +3,5 @@ local gameId = game.GameId
 if gameId == 8204899140 then
 
 end
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/LucidHubPremium/LucidPremium/main/Lucid-Premium-FF2"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LucidHubPremium/LucidPremium/main/Lucid-Premium-FF2.lua"))()
    
