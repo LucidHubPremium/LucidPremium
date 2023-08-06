0dd7277564782c9c031819b6340f26f74cb45740
LucidHubPremium <121714664+LucidHubPremium@users.noreply.github.com>
2023-08-06 14:11:34 -0700

    Update LucidPremiumMain.lua

--- LucidPremiumMain.lua
+++ LucidPremiumMain.lua
@@ -1,7 +1,16 @@
+--Lucid Premium
 local gameId = game.GameId
 
-if gameId == 8204899140 then
+if gameId == 3150475059 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LucidHubPremium/LucidPremium/main/Lucid-Premium-FF2.lua"))()
+    print("Lucid Premium FF2 Loaded")
+
+
+elseif gameId == 2766122154 then
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/LucidHubPremium/LucidPremium/main/Lucid-Premium-FBDuels.lua"))()
+    print("Lucid Premium FB Duels Loaded")
+
 
 end
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/LucidHubPremium/LucidPremium/main/Lucid-Premium-FF2.lua"))()
-   
