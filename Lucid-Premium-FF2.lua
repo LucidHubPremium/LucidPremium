202a97086f3bd22a36166156ee26b53a7cd0c2e1
LucidHubPremium <121714664+LucidHubPremium@users.noreply.github.com>
2023-08-14 19:15:21 -0700

    Update Lucid-Premium-FF2.lua

--- Lucid-Premium-FF2.lua
+++ Lucid-Premium-FF2.lua
@@ -1,4 +1,129 @@
 
+local CoreGui = Instance.new("ScreenGui")
+local Coreloader = Instance.new("Frame")
+local Loader = Instance.new("Frame")
+local Thing = Instance.new("Frame")
+local TextLabel2 = Instance.new("TextLabel")
+local TextLabel = Instance.new("TextLabel")
+local TextButton = Instance.new("TextButton")
+ 
+CoreGui.Name = "CoreGui"
+CoreGui.Parent = game.CoreGui
+CoreGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Coreloader.Name = "Coreloader"
+Coreloader.Parent = CoreGui
+Coreloader.BackgroundColor3 = Color3.new(0.211765, 0.211765, 0.211765)
+Coreloader.BorderColor3 = Color3.new(0, 0, 0)
+Coreloader.BorderSizePixel = 0
+Coreloader.Position = UDim2.new(0.35, 0, 0.36807102, 0)
+Coreloader.Size = UDim2.new(0, 568, 0, 239)
+
+Loader.Name = "Loader"
+Loader.Parent = Coreloader
+Loader.BackgroundColor3 = Color3.new(0.372549, 0.117647, 0.458824)
+Loader.BorderColor3 = Color3.new(0, 0, 0)
+Loader.BorderSizePixel = 0
+Loader.Position = UDim2.new(0.0528169014, 0, 0.815899551, 0)
+Loader.Size = UDim2.new(0, 507, 0, 8)
+
+Thing.Name = "Thing"
+Thing.Parent = Loader
+Thing.BackgroundColor3 = Color3.new(1, 0.172549, 1)
+Thing.BorderColor3 = Color3.new(0, 0, 0)
+Thing.BorderSizePixel = 0
+Thing.Size = UDim2.new(0, 0, 0, 8)
+
+TextLabel2.Name = "TextLabel2"
+TextLabel2.Parent = Coreloader
+TextLabel2.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel2.BackgroundTransparency = 1
+TextLabel2.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel2.BorderSizePixel = 0
+TextLabel2.Position = UDim2.new(0.0827464759, 0, 0.125523016, 0)
+TextLabel2.Size = UDim2.new(0, 473, 0, 50)
+TextLabel2.Font = Enum.Font.Gotham
+TextLabel2.Text = "Lucid"
+TextLabel2.TextColor3 = Color3.new(1, 1, 1)
+TextLabel2.TextSize = 28
+
+TextLabel.Parent = Coreloader
+TextLabel.BackgroundColor3 = Color3.new(1, 1, 1)
+TextLabel.BackgroundTransparency = 1
+TextLabel.BorderColor3 = Color3.new(0, 0, 0)
+TextLabel.BorderSizePixel = 0
+TextLabel.Position = UDim2.new(0.107394367, 0, 0.882845163, 0)
+TextLabel.Size = UDim2.new(0, 441, 0, 19)
+TextLabel.Font = Enum.Font.Gotham
+TextLabel.Text = "Please wait loading wont take long..."
+TextLabel.TextColor3 = Color3.new(1, 1, 1)
+TextLabel.TextSize = 14
+
+TextButton.Parent = Coreloader
+TextButton.BackgroundColor3 = Color3.new(0.176471, 0.176471, 0.176471)
+TextButton.BorderColor3 = Color3.new(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0.383802831, 0, 0.430962354, 0)
+TextButton.Size = UDim2.new(0, 130, 0, 50)
+TextButton.Font = Enum.Font.Gotham
+TextButton.Text = "Copy Discord Link"
+TextButton.TextColor3 = Color3.new(1, 1, 1)
+TextButton.TextSize = 14
+
+
+
+local frame = game:GetService("CoreGui").CoreGui.Coreloader.Loader.Thing
+local guisize = UDim2.new(0, 509,0, 8)
+local tween_time = math.random(4,5)
+local tween_time2 = 2
+local frame2 = game:GetService("CoreGui").CoreGui.Coreloader
+local guipos = UDim2.new(0.35, 0,-0.500, 0)
+local button = game:GetService("CoreGui").CoreGui.Coreloader.TextButton
+
+button.MouseButton1Click:Connect(function()
+setclipboard("https://discord.gg/hwmCFgppQH")
+end)
+
+wait(0.1)	
+
+function namechange1()
+	game:GetService("CoreGui").CoreGui.Coreloader.TextLabel2.Text = "Checking Whitelist..."
+end
+
+function namechange2()
+	game:GetService("CoreGui").CoreGui.Coreloader.TextLabel2.Text = "Authenticating..."
+end
+
+function namechange3()
+	game:GetService("CoreGui").CoreGui.Coreloader.TextLabel2.Text = "Bypassing..."
+end
+
+function starttween()
+	local tweenInfo = TweenInfo.new(tween_time, Enum.EasingStyle.Linear)
+	local tween = game:GetService("TweenService"):Create(frame, tweenInfo, {Size = guisize})
+	tween:Play()
+end	
+wait(0.5)
+starttween()
+wait(1)
+namechange1()
+wait(1.2)
+namechange2()
+wait(1.4)
+namechange3()
+wait(2.4)
+
+function endtween2()
+	local tweenInfo2 = TweenInfo.new(tween_time2, Enum.EasingStyle.Quad)
+	local tween2 = game:GetService("TweenService"):Create(frame2, tweenInfo2, {Position = guipos})
+	tween2:Play()
+end
+endtween2()
+
+wait(10)
+
+	game:GetService("CoreGui").CoreGui:Destroy()
+
 
 
 do --//
@@ -88,7 +213,7 @@ local tab = win:Tab("Player")
 local Playeer = game.Players.LocalPlayer
 _G.CheckingTool = false
 
-tab:Toggle("Long Arms", {Toggled=false , Description = false}, function(bool)
+tab:Toggle("Long Arms", false, function(bool)
    _G.CheckingTool = bool
 Highlight = Instance.new("Highlight", Playeer.Character['Left Arm'])
 Highlight.Enabled = bool
@@ -119,8 +244,8 @@ end
 end)
 
 
-tab:Toggle("Long Legs", {Toggled=false , Description = false}, function(bool)
-   __G.CheckingTool = bool
+tab:Toggle("Long Arms", false, function(bool)
+   _G.CheckingTool = bool
 Highlight = Instance.new("Highlight", Playeer.Character['Left Leg'])
 Highlight.Enabled = bool
 Highlight = Instance.new("Highlight", Playeer.Character['Right Leg'])
@@ -140,24 +265,12 @@ end
 end)
 
 tab:Slider("Long Arms Strength", 1, 40, 20, function(g)
-   _G.Arms = g
-if _G.CheckingTool == true then
-Playeer.Character['Left Arm'].Size = Vector3.new(1, _G.Arms, 1)
-Playeer.Character['Right Arm'].Size = Vector3.new(1, _G.Arms, 1)
-elseif _G.CheckingTool == false then
-
-end
-end)
-
-
-
-tab:Slider("Long Legs Height", 1, 40, 20, function(g)
    _G.Legs = g
 if _G.CheckingTool == true then
 Playeer.Character['Left Leg'].Size = Vector3.new(1, _G.Legs, 1)
 Playeer.Character['Right Leg'].Size = Vector3.new(1, _G.Legs, 1)
 elseif _G.CheckingTool == false then
-		end
+
 end
 end)
 
